ef9424c5a5c5048263aa8cd0521529828b661ff0
Monira Jahan <monirajahan459@gmail.com>
2023-08-05 13:40:53 +0600

    Animation Added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,20 +1,27 @@
+import { Slide } from "react-awesome-reveal";
+
 
 const Banner = () => {
     return (
         <div className="carousel w-full h-[200px] md:h-[500px] rounded-xl mt-10 md:hover:-translate-y-6 ">
             <div id="slide1" className="carousel-item relative w-full bg-gradient-to-r from-purple-300 to-slate-300  ">
-
+           
                 <div className="absolute flex items-center left-0 top-0  bg-gradient-to-r from-pink-400 to-purple-300 h-full">
+                   
                     <div className='text-white space-y-7 w-1/2 md:px-24 md:border-teal-400 md:shadow-2xl md:rounded-xl py-4 md:py-12'>
-                        <h2 className='md:text-4xl text-black font-bold '>Best Disney Toy Store For Kids</h2>
-                        <p className="font-normal text-sm md:text-xl text-black ">The Disney Store is a chain of specialty stores selling only Disney related items, many of them exclusive, under its own name and Disney Outlet.</p>
+                        <Slide>
+                        <h2 className='md:text-2xl text-black font-bold '>Best Disney Toy Store For Kids</h2>
+                        <p className="text-black">The Disney Store is a chain of specialty stores selling only Disney related items, many of them exclusive, under its own name and Disney Outlet.</p>
                         <div>
                             <button className="btn bg-purple-600 border-none mr-5">Shop Now</button>
                             
                         </div>
+                        </Slide>
                     </div>
                     <img src="https://i.ibb.co/qxBTrHB/7-2-disney-princesses-free-download-png.png" className="w-2/5 h-96" />
+                    
                 </div>
+              
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle  bg-purple-600 border-none ">❮</a>
                     <a href="#slide2" className="btn btn-circle  bg-purple-600 border-none">❯</a>
@@ -24,17 +31,19 @@ const Banner = () => {
 
                 <div className="absolute flex items-center left-0 top-0  bg-gradient-to-r from-pink-400 to-purple-300 h-full">
                     <div className='text-black space-y-7 w-1/2 px-20 border-teal-400 shadow-2xl rounded-xl py-12'>
-                        <h2 className='text-4xl  font-bold '>Best Disney Toy Store For Kids</h2>
+                        <Slide>
+                        <h2 className='text-2xl  font-bold '>Best Disney Toy Store For Kids</h2>
                         <p>The Disney Store is a chain of specialty stores selling only Disney related items, many of them exclusive, under its own name and Disney Outlet. It was a business unit of Disney Consumer Products with the Disney Parks, Experiences and Products segment of The Walt Disney Company conglomerate.</p>
                         <div>
                             <button className="btn bg-purple-600 border-none mr-5">Shop Now</button>
                            
                         </div>
+                        </Slide>
                     </div>
-
+                 
                     <img src="https://i.ibb.co/McdHCzn/751589-mickey-mouse.jpg" className=" border-white border-8 w-1/4 rounded-lg shadow-2xl ml-8"/>
                     <img src="https://i.ibb.co/5Mn9KSG/Easy-Disney-Drawing-Ideas-And-Tutorials.jpg" className="w-1/4 rounded-lg shadow-2xl absolute right-10 top-1/2 border-white border-8" />
-
+                   
                 </div>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle  bg-purple-600 border-none mb-14 ">❮</a>
@@ -45,12 +54,14 @@ const Banner = () => {
 
                 <div className="absolute flex items-center left-0 top-0  bg-gradient-to-r from-pink-400 to-purple-300 h-full">
                     <div className='text-black space-y-7 w-1/2 px-20 border-teal-400 shadow-2xl rounded-xl py-12'>
-                        <h2 className='text-4xl font-bold '>Best Disney Toy Store For Kids</h2>
+                    <Slide>
+                        <h2 className='text-2xl font-bold '>Best Disney Toy Store For Kids</h2>
                         <p>The Disney Store is a chain of specialty stores selling only Disney related items, many of them exclusive, under its own name and Disney Outlet. It was a business unit of Disney Consumer Products with the Disney Parks, Experiences and Products segment of The Walt Disney Company conglomerate.</p>
                         <div>
                             <button className="btn bg-purple-600 border-none mr-5">Shop Now</button>
                            
                         </div>
+                        </Slide>
                     </div>
 
                     <img src="https://www.seekpng.com/png/detail/412-4129751_toyst-toy-story-complete-characters-png.png" className="w-2/5 h-96 border-8 rounded-xl ml-8" />
--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -5,12 +5,12 @@ const Gallery = () => {
 
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 h-full">
-                <img className="bg-purple-700  transition duration-300  ease-in-out hover:opacity-60 card w-96 border-8 border-pink-400 shadow-2xl" src="https://i.ibb.co/G3YPphz/img1.jpg" alt="" />
-                <img className="bg-blue-400 bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-8 shadow-2xl border-pink-400 h-96" src="https://i.ibb.co/G73wJfL/img2.jpg" alt="" />
-                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-8 shadow-2xl border-pink-400" src="https://i.ibb.co/c82tWNf/img6.jpg" alt="" />
-                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-8 shadow-2xl border-pink-400 h-96" src="https://i.ibb.co/VSDxp5h/img8.jpg" alt="" />
-                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-8 shadow-2xl border-pink-400" src="https://i.ibb.co/QftXtMH/donald-duck.jpg" alt="" />
-                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-8 shadow-2xl border-pink-400 h-96" src="https://i.ibb.co/7KbdLQb/img5.jpg" alt="" />
+                <img className="h-auto max-w-full rounded-lg bg-purple-700  transition duration-300  ease-in-out hover:opacity-60 card w-96 border-4 border-pink-400 shadow-2xl" src="https://i.ibb.co/G3YPphz/img1.jpg" alt="" />
+                <img className="bg-blue-400 bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-4 shadow-2xl border-pink-400 h-96" src="https://i.ibb.co/G73wJfL/img2.jpg" alt="" />
+                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-4 shadow-2xl border-pink-400" src="https://i.ibb.co/c82tWNf/img6.jpg" alt="" />
+                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-4 shadow-2xl border-pink-400 h-96" src="https://i.ibb.co/VSDxp5h/img8.jpg" alt="" />
+                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-4 shadow-2xl border-pink-400" src="https://i.ibb.co/QftXtMH/donald-duck.jpg" alt="" />
+                <img className="bg-fixed  transition duration-300 ease-in-out hover:opacity-60 card w-96 border-4 shadow-2xl border-pink-400 h-96" src="https://i.ibb.co/7KbdLQb/img5.jpg" alt="" />
             </div>
             <div className="md:mx-96 my-8">
             <button className="btn bg-purple-500 mx-36 border-none">See More</button>
